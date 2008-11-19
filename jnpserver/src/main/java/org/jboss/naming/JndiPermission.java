68f93647a19fb1defec10e9d708c1f63b17b99bd
scott.stark@jboss.org <scott.stark@jboss.org@84be2c1e-ba19-0410-b317-a758671a6fc1>
2008-11-19 18:20:31 +0000

    Correct the copyright header usage per counsel from Richard Fontana(rfontana@redhat.com)
    git-svn-id: http://anonsvn.jboss.org/repos/jbossas/projects/naming/trunk@81309 84be2c1e-ba19-0410-b317-a758671a6fc1

--- jnpserver/src/main/java/org/jboss/naming/JndiPermission.java
+++ jnpserver/src/main/java/org/jboss/naming/JndiPermission.java
@@ -1,4 +1,9 @@
-/*
+/* 
+ * JBoss, Home of Professional Open Source
+ * Copyright 2008, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
  * Copyright 1997-2005 Sun Microsystems, Inc.  All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
@@ -21,28 +26,29 @@
  * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
  * CA 95054 USA or visit www.sun.com if you need additional information or
  * have any questions.
+ *
+ * The text of the Classpath Exception, as provided by Sun
+ * Microsystems, Inc. to the original version of this code, and
+ * extended by Red Hat Middleware LLC to its modifications, follows:
+ * 
+ * Linking this library statically or dynamically with other modules
+ * is making a combined work based on this library. Thus, the terms
+ * and conditions of the GNU General Public License cover the whole
+ * combination.
+ *
+ * As a special exception, the copyright holders of this library give
+ * you permission to link this library with independent modules to
+ * produce an executable, regardless of the license terms of these
+ * independent modules, and to copy and distribute the resulting
+ * executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the
+ * license of that module. An independent module is a module which is
+ * not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the
+ * library, but you are not obligated to do so. If you do not wish to
+ * do so, delete this exception statement from your version. 
  */
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2008, Red Hat Middleware LLC, and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- */
+
 package org.jboss.naming;
 
 import java.security.Permission;
